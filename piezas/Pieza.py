eaeda89434e3da4fadb13b6ff46325813ef05650
fernandopaz1 <ferpaz9292@gmail.com>
2021-07-08 01:43:44 -0300

    simplificando objeto color a boolean

--- piezas/Pieza.py
+++ piezas/Pieza.py
@@ -1,9 +1,8 @@
 from abc import abstractmethod
-from . import Color
-class Pieza(Color):
+class Pieza:
     def __init__(self, position, color):
         self.__position = position
-        self.__color = Color(color)
+        self.__color = color
         self.__casillas_amenazadas  = []
         pass
 
@@ -31,4 +30,4 @@ class Pieza(Color):
         return self.__class__ == other.__class__ and self.__position == other.__position and self.__color == other
 
     def __hash__(self):
-        return hash(self.color) + hash(self.position)
+        return hash(self.color()) + hash(self.position())
