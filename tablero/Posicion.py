9c4258a9cc0d7811c1cf1ec8f225362126f35352
fernandopaz1 <ferpaz9292@gmail.com>
2021-07-26 07:46:43 -0300

    feat: formateando clases con autopep8

--- tablero/Posicion.py
+++ tablero/Posicion.py
@@ -1,14 +1,16 @@
 class Posicion:
-    dic_col = {"a" : 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8}
-    dic_col_reverse = {1: "a", 2: "b", 3: "c", 4: "d", 5: "e", 6: "f", 7: "g", 8: "h"}
+    dic_col = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8}
+    dic_col_reverse = {1: "a", 2: "b", 3: "c",
+                       4: "d", 5: "e", 6: "f", 7: "g", 8: "h"}
+
     def __init__(self, fila, columna):
-        if fila<=0 or fila>8:
+        if fila <= 0 or fila > 8:
             raise Exception("Filas fuera de rango")
-        if columna.toLower()<"a" or columna.toLower()>"h": 
+        if columna.toLower() < "a" or columna.toLower() > "h":
             raise Exception("Columna fuera de rango")
-        self.__fila= fila
-        self.__columna=Posicion.dic_col.get(columna)
-    
+        self.__fila = fila
+        self.__columna = Posicion.dic_col.get(columna)
+
     def __str__(self):
         return Posicion.dic_col.get(self.columna) + self.fila
 
@@ -20,9 +22,8 @@ class Posicion:
     def columna(self):
         return self.__columna
 
-
     def __eq__(self, other):
         return self.__class__ == other.__class__ and self.fila() == other.fila() and self.columna() == other.columna()
-    
+
     def __hash__(self):
         return hash(self.fila()) + hash(self.columna())
--- tablero/Tablero.py
+++ tablero/Tablero.py
@@ -1,30 +1,33 @@
 from . import Posicion
+
+
 class Tablero:
     def __init__(self):
         self.__tablero = {}
-    
+
     @property
     def tablero(self):
         return self.__tablero
 
     def agregar(self, pieza, posicion):
-        if self.__tablero.put(posicion, pieza):
+        if self.tablero[posicion] is not None:
+            self.tablero[posicion] = pieza
             pieza.set_position(posicion)
-    
+
     def quitar(self, posicion):
-        self.tablero().pop(posicion)
+        self.tablero[posicion] = None
 
     def mover(self, pieza, posicion):
         if pieza.puede_moverse_a(posicion):
             aux = self.pieza_en(posicion)
-            if aux == None or not pieza.mismo_color(aux):
+            if aux is None or not pieza.mismo_color(aux):
                 self.quitar(posicion)
                 self.agregar(pieza, posicion)
 
     def pieza_en(self, posicion):
-        return self.tablero().get(posicion, None)
-
+        return self.tablero.get(posicion, None)
 
 
 if __name__ == '__main__':
     print("Este es el tablero")
+
